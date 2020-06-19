3d6973e68e66c70e7e5e56363a89d677d7c5bfea
MestrieEsteban <esteban94.em@gmail.com>
2020-06-19 13:56:07 +0200

    position des valeurs du maze

--- build/maze.js
+++ build/maze.js
@@ -1,1 +1,46 @@
-console.log('ok');
+"use strict";
+exports.__esModule = true;
+var fs_1 = require("fs");
+var Maze = /** @class */ (function () {
+    function Maze() {
+        this.mazeWithPosition = {};
+    }
+    return Maze;
+}());
+function solveMaze() {
+    console.time('Temps');
+    var maze = myMaze(process.argv[2]);
+    console.timeEnd('Temps');
+    console.log(maze);
+}
+function myMaze(mazePath) {
+    var mazeRaw = fs_1.readFileSync(mazePath, 'utf8');
+    var mazeLine = mazeRaw.split('\n');
+    var maze = new Maze();
+    maze.mazeSize = mazeLine[0].split("x");
+    var y = -1;
+    for (var _i = 0, mazeLine_1 = mazeLine; _i < mazeLine_1.length; _i++) {
+        var line = mazeLine_1[_i];
+        var x = -1;
+        y++;
+        if (!line) {
+            break;
+        }
+        for (var _a = 0, _b = line.split(''); _a < _b.length; _a++) {
+            var value = _b[_a];
+            x++;
+            var position = x + ":" + y;
+            maze.mazeWithPosition[position] = {
+                path: value
+            };
+            if (value == "1") {
+                maze.mazeStart = position;
+            }
+            if (value == "2") {
+                maze.mazeEnd = position;
+            }
+        }
+    }
+    return maze;
+}
+solveMaze();
--- src/maze.ts
+++ src/maze.ts
@@ -1,1 +1,52 @@
-console.log('ok')
+import { readFileSync } from "fs";
+import { mainModule } from "process";
+
+class Maze {
+    mazeSize:       	Array<string>;
+    mazeWithPosition:   object = {};
+    mazeStart:      	string;
+	mazeEnd:        	string;
+
+
+}
+
+function solveMaze(){
+	console.time('Temps')
+
+	const maze = myMaze(process.argv[2]);
+	console.timeEnd('Temps')
+	console.log(maze)
+}
+function myMaze(mazePath: string) {
+    let mazeRaw = readFileSync(mazePath, 'utf8');
+    let mazeLine = mazeRaw.split('\n');
+
+    let maze = new Maze();
+    
+    maze.mazeSize = mazeLine[0].split("x")
+    
+    let y:number = -1;
+	for (let line of mazeLine) {
+		let x:number = -1;
+		y++;
+		if (!line) { break; }
+		for (let value of line.split('')) {
+			x++
+			let position = `${x}:${y}`;
+			maze.mazeWithPosition[position] = {
+				path: value,
+			}
+			if(value == "1"){
+				maze.mazeStart = position;
+			}
+			if(value == "2"){
+				maze.mazeEnd = position;
+			}			
+		}
+	}
+
+	return maze
+}
+
+solveMaze()
+
